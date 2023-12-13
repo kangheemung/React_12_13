260bcc915d3015aed9fcbb4c8bfe2d298e11888a
EC2 Default User <ec2-user@ip-10-0-0-125.ap-northeast-1.compute.internal>
2023-12-13 13:30:10 +0000

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import logo from './logo.svg'; // Assuming you have this image.
 import './App.css';
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+  
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+  };
+  
+  const addTask = () => {
+    if(newTask !== "") { // Prevent empty tasks from being added.
+      setTodoList([...todoList, newTask]);
+      setNewTask(""); // Clear the input after adding a task.
+    }
+  };
+  
+  const deleteTask = taskToDelete => {
+    setTodoList(todoList.filter(task => task !== taskToDelete));
+  };
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input value={newTask} onChange={handleChange} />
+        <button onClick={addTask}>Add Task</button>
+        <div>
+          {todoList.map((task, index) => (
+            <div key={index}> {/* Added key for list efficiency and to avoid warnings */ }
+              <h1>{task}</h1>
+              <button onClick={() => deleteTask(task)}>x</button>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
